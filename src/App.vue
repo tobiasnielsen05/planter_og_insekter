9498173787c92c337812f9e360f005936fd94e01
Tobias <ton001@edu.zealand.dk>
2025-11-06 23:08:23 +0100

    Tilføjet billede og ændret i css'en for at tilpasse lidt mere i forhold til vores mockup

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,27 @@
-<template></template>
+<template>
+  <section class="top-nav">
+    <header class="top-nav">
+      <button class="menu-btn" aria-label="Åben menu">
+        <span></span><span></span><span></span>
+      </button>
+    </header>
+  </section>
+
+  <section class="hero">
+    <div class="hero-content">
+          <h1>Grøn<br>Fremtid</h1>
+        </div>
+
+        <div class="mission-box">
+          <p>Vores budskab er dette</p>
+          <button class="mission-btn">Vores mission</button>
+        </div>
+  </section>
+
+</template>
+
+
+<script setup>
+// importer styling
+import '@/styles/hero.css'
+</script>
